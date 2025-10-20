348fccc8729ce6228db3a3980705803e6b7a3192
Jacob Solberg Holm <jas@equinor.com>
2025-10-20 09:16:10 +0200

    .

--- runAsUserJob/main.go
+++ runAsUserJob/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strconv"
+	"log"
+	"time"
 )
 
 func printUser() {
-	fmt.Println("UID: " + strconv.Itoa(os.Getuid()))
-	fmt.Println("GID: " + strconv.Itoa(os.Getgid()))
-	fmt.Println("test bare")
+	// fmt.Println("UID: " + strconv.Itoa(os.Getuid()))
+	// fmt.Println("GID: " + strconv.Itoa(os.Getgid()))
+	// fmt.Println("test bare")
+
+	log.Println("hello world")
 }
 
 func main() {
 	printUser()
 
-	//time.Sleep(10 * time.Minute)
+	time.Sleep(10 * time.Minute)
 }
